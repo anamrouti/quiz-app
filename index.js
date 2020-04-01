5bf6d88c3dca41922beb1045867cde3df1323e27
anamrouti <57967377+anamrouti@users.noreply.github.com>
2020-04-01 18:03:08 -0400

    improvements

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+const STORE = [
+    {   //1
+        question: 'What are the symptoms a person infected with COVID-19 shows?',
+        answers: ['Unusual Fever', 'Cough', 'Shortness of breath and/or chest pain', 'All the above'],
+        correctAnswer: 'All the above'
+    },
+    {   //2
+        question: 'How long does it take to see symptoms if a person has been infected?',
+        answers: ['1-2 days', '2-4 days', '2-14 days', '2-4 weeks'],
+        correctAnswer: '2-14 days'
+    },
+    {   //3
+        question: 'How does the virus spread from one person to another?',
+        answers: ['Airborne: breathing the same air as infected person', 'Through respiratory droplets produced when an infected person coughs or sneezes',
+           'Touching infected surfaces and washing hands immediately without touching face', 'Wearing face-mask even without noticing any symptoms' ],
+        correctAnswer: 'Through respiratory droplets produced when an infected person coughs or sneezes'
+    },
+    {   //4
+        question: 'How can you protect yourself from being infected?',
+        answers: ['Washing hands consistently after touching potentially infected surfaces', 'Keep a safe (6ft) distance from other persons who show symptoms',
+        'Wear a face-mask if you show symptoms of illness', 'All the above'],
+        correctAnswer: 'All the above'
+    },
+    {   //5
+        question: 'Who is most vulnerable to become very sick from this illness?',
+        answers: ['Anyone who comes in contact with the virus', 'Children under the age of 6',
+        'Older Adults', 'People who have serious health conditions such as: Heart disease, Diabetes, or Lung disease'],
+        correctAnswer: 'People who have serious health conditions such as: Heart disease, Diabetes, or Lung disease'
+    },
+    {   //6
+        question: 'What percentage of people who are exposed to COVID-19 cure from it without taking any specific medication?',
+        answers: ['45% of people infected', '60% of people infected', '80% of people cure from COVID-19 without medication', '95% of people infected'],
+        correctAnswer: '80% of people cure from COVID-19 without medication'
+    },
+    {   //7
+        question: 'Corona Virus is know to be a deadly flu virus, what percentage of infected cases end up reaching severe illness?',
+        answers: ['20% of victims become seriously ill', '10% of victims become seriously ill', '5% of victims become seriously ill',
+        'less than 3% of victims actually become seriously ill'],
+        correctAnswer: 'less than 3% of victims actually become seriously ill'
+    },
+]
+
 let score = 0;
 let questionNumber = 0;
     //generates questions until STORE length is reached
@@ -77,8 +119,7 @@ function correctAnswer(){
 }
     //provides correct answer if answer choice doesn't match correct answer
 function wrongAnswer(){
-    $('.response').html(`<img class = "images" src = "C:\Users\amjad\Desktop\Clinics_WrongAnswer.jpg">
-    <p class= "correction">Oops! The Correct Answer is:</p>
+    $('.response').html(`<img class = "images" src = "C:\Users\amjad\Desktop\Clinics_WrongAnswer.jpg"><p class= "correction">Oops! The Correct Answer is:</p>
     <p class= "correctAnswer">${STORE[questionNumber].correctAnswer}</p>
     <button type = "submit" class = "nextButton button">Next Question</button>`);
 }
